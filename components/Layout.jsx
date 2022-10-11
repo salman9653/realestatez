31a5cd1ee6c4398fb5be019a26a1d508b6dd1f1b
salman9653 <salman9915189734@gmail.com>
2022-10-11 17:34:38 +0530

    added layout and completed home page

---
+++ components/Layout.jsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import { Box } from "@chakra-ui/react";
+
+
+const Layout = ({ children }) => {
+    return (
+        <>
+            <Head>
+                <title>Real Estate Z</title>
+            </Head>
+            <Box maxWidth="1280px" m="auto">
+                <header>
+                    Navbar
+                </header>
+                <main>
+                    {children}
+                </main>
+                <footer>
+                    Footer
+                </footer>
+            </Box>
+        </>
+    )
+}
+
+export default Layout
