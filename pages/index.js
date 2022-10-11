b1ac638763b1b60a344cc639574b81c2e136aae4
salman9653 <salman9915189734@gmail.com>
2022-10-11 15:36:39 +0530

    Created Property Component

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
-// import Property from '../components/Property';
+import Property from '../components/Property';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
@@ -32,9 +32,9 @@ const Home = ({ propertiesForSale, propertiesForRent }) => {
         linkName='/search?purpose=for-rent'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
       />
-      {/* <Flex flexWrap='wrap'>
+      <Flex flexWrap='wrap'>
         {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
-      </Flex> */}
+      </Flex>
       <Banner
         purpose='BUY A HOME'
         title1=' Find, Buy & Own Your'
@@ -45,9 +45,9 @@ const Home = ({ propertiesForSale, propertiesForRent }) => {
         linkName='/search?purpose=for-sale'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
       />
-      {/* <Flex flexWrap='wrap'>
+      <Flex flexWrap='wrap'>
         {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
-      </Flex> */}
+      </Flex>
     </Box>
   );
 }
